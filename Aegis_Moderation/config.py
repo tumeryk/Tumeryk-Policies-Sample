46408481e9952f844800b353240f8a7fc0d62d60
Tumeryk Configs <chitti.keswani@tumeryk.com>
2025-01-28 11:12:53 -0800

    Delete Aegis_Moderation directory

--- Aegis_Moderation/config.py
+++
@@ -1,7 +0,0 @@
-
-import os
-import pathlib
-current = pathlib.Path(__file__).parent.resolve()
-credential_path = f"{current}/google-creds.json"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
-      
